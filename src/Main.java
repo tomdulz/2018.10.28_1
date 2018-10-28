66e44c6356f0369893f5c6646847311d60520445
tomdulz <tomasz.dulz@gmail.com>
2018-10-28 15:54:07 +0100

    1st while loop to print 5 starts one below another

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main (String[] args)
+    {
+        //Task1
+        System.out.println("Task 1 - while");
+        int starLimit = 5;
+        int x = 1;
+        while(x <= starLimit){
+            System.out.print("*");
+        x++;}
+        System.out.println("");
+        // Task 2
+        System.out.println("Task 2 - while");
+        x = 1;
+        while(x <= starLimit){
+            System.out.println("*");
+            x++; }
+         // Task 3
+        System.out.println("Task 3");
+        int a = 1;
+        int b = 1;
+        int starRowLimit = 5;
+        int starColLimit = 5;
+        while(b <= starColLimit) {
+            while (a <= starRowLimit) {
+                System.out.print("*");
+                a++;
+            }
+            a = 1;
+            System.out.println("*");
+            b++;
+        }
+    }
+}
